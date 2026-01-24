f76a98701d34bf0e605b943e813abc7e87230aa3
William Crouch <richardwilliamcrouch@gmail.com>
2026-01-24 13:49:33 -0500

    Pre Command Factory Set Up

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -1,9 +1,12 @@
 package frc.robot.subsystems.shooter;
 
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 
 public class ShooterConstants {
-    
+
+  private ShooterConstants(){};
+  
   // TODO: Change to actual ports
   public static final int FLYWHEEL_MOTOR_ID = 4;
   public static final int HOOD_MOTOR_ID = 5;
@@ -16,7 +19,7 @@ public class ShooterConstants {
   public static final double FLYWHEEL_KP = 0;
   public static final double FLYWHEEL_KD = 0;
 
-  public static Slot0Configs createRollerMotorSlot0Configs() {
+  public static Slot0Configs createFlywheelMotorSlot0Configs() {
     Slot0Configs slot = new Slot0Configs();
     slot.kS = FLYWHEEL_KS;
     slot.kV = FLYWHEEL_KV;
@@ -25,5 +28,28 @@ public class ShooterConstants {
     return slot;
   }
 
-  
+  public static final double ALLOWABLE_HOOD_ERROR = 0.1;
+  public static final double HOOD_FORWARD_LIMIT = 100;
+  public static final double HOOD_REVERSE_LIMIT = 0;
+  public static final double HOOD_KS = 0;
+  public static final double HOOD_KP = 0;
+  public static final double HOOD_KD = 0;
+
+  public static Slot0Configs createHoodMotorSlot0Configs(){
+    Slot0Configs slot = new Slot0Configs();
+    slot.kS = HOOD_KS;
+    slot.kP = HOOD_KP;
+    slot.kD = HOOD_KD;
+    return slot;
+  }
+
+  public static SoftwareLimitSwitchConfigs createHoodSoftwareLimitSwitchConfigs(){
+    SoftwareLimitSwitchConfigs configs = new SoftwareLimitSwitchConfigs();
+    configs.ForwardSoftLimitEnable = false;
+    configs.ReverseSoftLimitEnable = false;
+    configs.ForwardSoftLimitThreshold = HOOD_FORWARD_LIMIT;
+    configs.ReverseSoftLimitThreshold = HOOD_REVERSE_LIMIT;
+    return configs;
+  }
+
 }
--- src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterSubsystem.java
@@ -1,5 +1,44 @@
 package frc.robot.subsystems.shooter;
 
-public class ShooterSubsystem {
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
+import com.ctre.phoenix6.controls.PositionTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase{
+    private final TalonFX flywheelMotor;
+    private final TalonFX hoodMotor;
+
+    private AngularVelocity launchVelocityTarget; //Rotations Per Second
+    private VelocityVoltage launchControl;
+
+    private Angle hoodTarget; //Rotations
+    private PositionTorqueCurrentFOC hoodControl;
+
+
+    public ShooterSubsystem(){
+        flywheelMotor = new TalonFX(ShooterConstants.FLYWHEEL_MOTOR_ID);
+        hoodMotor = new TalonFX(ShooterConstants.HOOD_MOTOR_ID);
+
+        flywheelMotor.getConfigurator().apply(ShooterConstants.createFlywheelMotorSlot0Configs());
+        launchVelocityTarget = RotationsPerSecond.of(0);
+        launchControl = new VelocityVoltage(0);
+
+        hoodMotor.getConfigurator().apply(ShooterConstants.createHoodMotorSlot0Configs());
+        hoodMotor.getConfigurator().apply(ShooterConstants.createHoodSoftwareLimitSwitchConfigs());
+        hoodTarget = Rotations.of(0);
+        hoodControl = new PositionTorqueCurrentFOC(0);
+    }
+
+    @Override
+    public void periodic(){}
+    
+
     
 }

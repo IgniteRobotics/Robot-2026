ac6e089713d6463d0e52e130a6516bb6120caf86
William Crouch <richardwilliamcrouch@gmail.com>
2026-01-27 22:45:01 -0500

    Cleanup

--- src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerConstants.java
@@ -3,26 +3,25 @@ package frc.robot.subsystems.indexer;
 import com.ctre.phoenix6.configs.Slot0Configs;
 
 public class IndexerConstants {
-    
-    private IndexerConstants(){}
 
-    //TODO Replace with real id
-    public static final int INDEXER_MOTOR_ID = 6;
+  private IndexerConstants() {}
 
-    //TODO: Tune motor
+  // TODO Replace with real id
+  public static final int INDEXER_MOTOR_ID = 6;
 
-    public static final double INDEXER_KS = 0;
-    public static final double INDEXER_KV = 0;
-    public static final double INDEXER_KP = 0;
-    public static final double INDEXER_KD = 0;
+  // TODO: Tune motor
 
-    public static Slot0Configs createIndexerMotorSlot0Configs() {
-        Slot0Configs slot = new Slot0Configs();
-        slot.kS = INDEXER_KS;
-        slot.kV = INDEXER_KV;
-        slot.kP = INDEXER_KP;
-        slot.kD = INDEXER_KD;
+  public static final double INDEXER_KS = 0;
+  public static final double INDEXER_KV = 0;
+  public static final double INDEXER_KP = 0;
+  public static final double INDEXER_KD = 0;
+
+  public static Slot0Configs createIndexerMotorSlot0Configs() {
+    Slot0Configs slot = new Slot0Configs();
+    slot.kS = INDEXER_KS;
+    slot.kV = INDEXER_KV;
+    slot.kP = INDEXER_KP;
+    slot.kD = INDEXER_KD;
     return slot;
   }
-
 }
--- src/main/java/frc/robot/subsystems/indexer/IndexerPreferences.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerPreferences.java
@@ -3,9 +3,9 @@ package frc.robot.subsystems.indexer;
 import frc.robot.preferences.DoublePreference;
 
 public class IndexerPreferences {
-    
-    private IndexerPreferences(){}
 
-    public static DoublePreference indexSpeed =
-      new DoublePreference( "Indexer/Index Speed", 0.1); // in rotations per second
+  private IndexerPreferences() {}
+
+  public static DoublePreference indexSpeed =
+      new DoublePreference("Indexer/Index Speed", 0.1); // in rotations per second
 }

a9ea5b976741a936a049c476985bb5784f30b2d3
William Crouch <richardwilliamcrouch@gmail.com>
2026-02-01 15:12:31 -0500

    Clean up

--- src/main/java/frc/robot/subsystems/climber/ClimberPreferences.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberPreferences.java
@@ -4,6 +4,6 @@ import frc.robot.preferences.DoublePreference;
 
 final class ClimberPreferences {
 
-    protected static DoublePreference pullPosition =
-      new DoublePreference("Climber/Pull Down", 3); // in rotations
+  protected static DoublePreference pullPosition =
+      new DoublePreference("Climber/Pull Down (Rotations)", 3); // in rotations
 }

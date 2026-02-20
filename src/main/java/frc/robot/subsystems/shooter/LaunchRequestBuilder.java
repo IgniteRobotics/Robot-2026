3012dd11182bd50ebdde4440094bdcc11d3bb00a
Joshua Reddick <y0shi@noreply.github.com>
2026-02-20 09:34:19 -0500

    Initial Translation of parabolic algorithm.  Stubbed mapped algorithm

---
+++ src/main/java/frc/robot/subsystems/shooter/LaunchRequestBuilder.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.LinearVelocity;
+import java.util.function.Supplier;
+
+/** Add your docs here. */
+public abstract class LaunchRequestBuilder {
+
+  public record LaunchRequest(
+      Angle launchHoodTarget,
+      AngularVelocity launchVelocity,
+      LinearVelocity driveVelocity,
+      Rotation2d driveAngle) {}
+
+  Supplier<Pose3d> targetPose;
+
+  public LaunchRequestBuilder(Supplier<Pose3d> targetPose) {
+    this.targetPose = targetPose;
+  }
+
+  public abstract LaunchRequest createLaunchRequest();
+}
---
+++ src/main/java/frc/robot/subsystems/shooter/MappedLauchRequestBuilder.java
@@ -0,0 +1,122 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.shooter;
+
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
+import edu.wpi.first.math.interpolation.InverseInterpolator;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import frc.robot.statemachines.DriveState;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Twist2d;
+
+import java.util.function.Supplier;
+
+/** Add your docs here. */
+public class MappedLauchRequestBuilder extends LaunchRequestBuilder {
+
+  public MappedLauchRequestBuilder(Supplier<Pose3d> targetPose) {
+    super(targetPose);
+  }
+
+  private static final double minDistance;
+  private static final double maxDistance;
+  private static final double passingMinDistance;
+  private static final double passingMaxDistance;
+  private static final double phaseDelay;
+
+
+  // Launching Maps
+  private static final InterpolatingTreeMap<Double, Rotation2d> hoodAngleMap =
+      new InterpolatingTreeMap<>(InverseInterpolator.forDouble(), Rotation2d::interpolate);
+  private static final InterpolatingDoubleTreeMap flywheelSpeedMap =
+      new InterpolatingDoubleTreeMap();
+  private static final InterpolatingDoubleTreeMap timeOfFlightMap =
+      new InterpolatingDoubleTreeMap();
+
+  // Passing Maps
+  private static final InterpolatingTreeMap<Double, Rotation2d> passingHoodAngleMap =
+      new InterpolatingTreeMap<>(InverseInterpolator.forDouble(), Rotation2d::interpolate);
+  private static final InterpolatingDoubleTreeMap passingFlywheelSpeedMap =
+      new InterpolatingDoubleTreeMap();
+  private static final InterpolatingDoubleTreeMap passingTimeOfFlightMap =
+      new InterpolatingDoubleTreeMap();
+
+
+
+
+  // TODO: All of this is made up.  Need real numbers.
+  static {
+    minDistance = 1.34;
+    maxDistance = 5.60;
+    passingMinDistance = 0.0;
+    passingMaxDistance = 100000;
+    phaseDelay = 0.03;
+
+    hoodAngleMap.put(1.34, Rotation2d.fromDegrees(19.0));
+    hoodAngleMap.put(1.78, Rotation2d.fromDegrees(19.0));
+    hoodAngleMap.put(2.17, Rotation2d.fromDegrees(24.0));
+    hoodAngleMap.put(2.81, Rotation2d.fromDegrees(27.0));
+    hoodAngleMap.put(3.82, Rotation2d.fromDegrees(29.0));
+    hoodAngleMap.put(4.09, Rotation2d.fromDegrees(30.0));
+    hoodAngleMap.put(4.40, Rotation2d.fromDegrees(31.0));
+    hoodAngleMap.put(4.77, Rotation2d.fromDegrees(32.0));
+    hoodAngleMap.put(5.57, Rotation2d.fromDegrees(32.0));
+    hoodAngleMap.put(5.60, Rotation2d.fromDegrees(35.0));
+
+    flywheelSpeedMap.put(1.34, 210.0);
+    flywheelSpeedMap.put(1.78, 220.0);
+    flywheelSpeedMap.put(2.17, 220.0);
+    flywheelSpeedMap.put(2.81, 230.0);
+    flywheelSpeedMap.put(3.82, 250.0);
+    flywheelSpeedMap.put(4.09, 255.0);
+    flywheelSpeedMap.put(4.40, 260.0);
+    flywheelSpeedMap.put(4.77, 265.0);
+    flywheelSpeedMap.put(5.57, 275.0);
+    flywheelSpeedMap.put(5.60, 290.0);
+
+    timeOfFlightMap.put(5.68, 1.16);
+    timeOfFlightMap.put(4.55, 1.12);
+    timeOfFlightMap.put(3.15, 1.11);
+    timeOfFlightMap.put(1.88, 1.09);
+    timeOfFlightMap.put(1.38, 0.90);
+
+    passingHoodAngleMap.put(passingMinDistance, Rotation2d.fromDegrees(0.0));
+    passingHoodAngleMap.put(passingMaxDistance, Rotation2d.fromDegrees(0.0));
+
+    passingFlywheelSpeedMap.put(passingMinDistance, 0.0);
+    passingFlywheelSpeedMap.put(passingMaxDistance, 0.0);
+
+    passingTimeOfFlightMap.put(passingMinDistance, 0.0);
+    passingTimeOfFlightMap.put(passingMaxDistance, 0.0);
+  }
+
+
+  @Override
+  public LaunchRequest createLaunchRequest() {
+
+    //current pose and movement
+    Pose2d estimatedRobotPose = DriveState.getInstance().getCurrentDriveStats().Pose;
+    ChassisSpeeds robotSpeeds = DriveState.getInstance().getCurrentDriveStats().Speeds;
+
+    //predicted pose
+    estimatedRobotPose.exp(
+        new Twist2d(
+            robotSpeeds.vxMetersPerSecond * phaseDelay,
+            robotSpeeds.vyMetersPerSecond * phaseDelay,
+            robotSpeeds.omegaRadiansPerSecond * phaseDelay
+        )
+    );
+
+    //TODO:  for now assume they're the same.  calculate offsets later
+    Pose2d launcherPose = estimatedRobotPose;
+
+
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'createLaunchRequest'");
+  }
+}

1231f2e3f561c364499bc261dd1e1108a9572fde
Brandon <bbunnyeng0930@gmail.com>
2026-03-02 18:34:06 -0500

    closest_ball

--- src/main/python/vision/model-training/vision.py
+++ src/main/python/vision/model-training/vision.py
@@ -1,6 +1,7 @@
 import cv2
 from inference_sdk import InferenceHTTPClient
 from inference_sdk.webrtc import WebcamSource, StreamConfig, VideoMetadata
+import math
 
 # Initialize client
 client = InferenceHTTPClient.init(
@@ -43,3 +44,22 @@ def on_data(data: dict, metadata: VideoMetadata):
 
 # Run the session (blocks until closed)
 session.run()
+
+def closest_ball(predictions):
+    lemons = [p for p in predictions id p.get('class') == 'lemon' or p.get('class_name') == 'lemon']
+    if len(lemon) < 1:
+        return None, None, float('inf') # going for a single ball wouldnt be efficient
+    min_dist = float('inf')
+    closest_set = (None, None)
+
+    for i in range(len(lemon)):
+        for j in range(i + 1, len(lemon)):
+            p1, p2 = lemon[i], lemon[j]
+            # Euclidean distance
+            dist = math.sqrt((p2['x'] - p1['x'])**2 + (p2['y'] - p1['y'])**2)
+            
+            if dist < min_dist:
+                min_dist = dist
+                closest_pair = (p1, p2)
+                
+    return closest_pair[0], closest_pair[1], min_dist

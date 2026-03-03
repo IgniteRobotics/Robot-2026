ac6479138137052f099e748697be122751d3e4cc
TheGreatPintoJ <thegreatpintoj@gmail.com>
2026-03-02 22:08:37 -0500

    Log target robot pose and stuff

--- src/main/java/frc/robot/statemachines/LaunchState.java
+++ src/main/java/frc/robot/statemachines/LaunchState.java
@@ -3,7 +3,10 @@ package frc.robot.statemachines;
 import static edu.wpi.first.units.Units.Rotations;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.shooter.LaunchRequest;
 import frc.robot.subsystems.shooter.ShooterConstants;
@@ -18,13 +21,18 @@ public class LaunchState {
     return single_instance;
   }
 
+  AllianceState allianceState = AllianceState.getInstance();
   private LaunchCalculator launchCalculator = LaunchCalculator.getInstance();
+
+  @Logged(name = "Current Launch Reqeust")
   private LaunchRequest currentLaunchRequest = null;
+
   @Logged(name = "3D Target Pose")
   private Pose3d targetPose3d = ShooterConstants.RED_TARGET;
+
   @Logged(name = "2D Target Pose")
   private Pose2d targetPose2d = ShooterConstants.RED_TARGET.toPose2d();
-  
+
   private LaunchType builderType = LaunchType.MAPPED;
 
   public LaunchRequest getLaunchRequest() {
@@ -45,9 +53,23 @@ public class LaunchState {
         currentLaunchRequest.getFlywheelVelocity().in(RotationsPerSecond));
     SmartDashboard.putNumber(
         "Launch/Hood Target (Rotations)", currentLaunchRequest.getHoodTarget().in(Rotations));
+
+    Alliance alliance = allianceState.getAlliance();
+    if (alliance == Alliance.Red) {
+      setTargetPose3d(ShooterConstants.RED_TARGET);
+      setTargetPose2d(ShooterConstants.RED_TARGET.toPose2d());
+    } else {
+      setTargetPose3d(ShooterConstants.BLUE_TARGET);
+      setTargetPose2d(ShooterConstants.BLUE_TARGET.toPose2d());
+    }
   }
+
   public void setTargetPose3d(Pose3d target) {
     this.targetPose3d = target;
+  }
+
+  public void setTargetPose2d(Pose2d target) {
+    this.targetPose2d = target;
   }
 
   public void setBuilderType(LaunchType builderType) {

6220241b125a65738361940e86cc661e62d748de
William Crouch <richardwilliamcrouch@gmail.com>
2026-03-10 17:52:09 -0400

    wheel radius test added (need drive radius)

--- src/main/java/frc/robot/subsystems/drive/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/DrivetrainSubsystem.java
@@ -1,20 +1,28 @@
 package frc.robot.subsystems.drive;
 
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+
 import com.ctre.phoenix6.configs.ProximityParamsConfigs;
 import com.ctre.phoenix6.hardware.CANrange;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveModule.SteerRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.ctre.phoenix6.swerve.SwerveRequest.FieldCentric;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.generated.CommandSwerveDrivetrain;
 import frc.robot.generated.TunerConstants;
@@ -255,5 +263,91 @@ public class DrivetrainSubsystem extends CommandSwerveDrivetrain {
   public Command followPath(PathPlannerPath path) {
     this.resetPose(path.getStartingHolonomicPose().get());
     return AutoBuilder.followPath(path);
+  }
+
+  private static class WheelRadiusCharacterizationState {
+    double[] startingWheelPositions = new double[4];
+    Rotation2d lastHeading = Rotation2d.kZero;
+    double headingDelta = 0.0;
+  }
+
+  public Command wheelRadiusCharacterization() {
+    SlewRateLimiter limiter =
+        new SlewRateLimiter(DriveConstants.WHEEL_RADIUS_TEST_MAX_VELOCITY.in(RadiansPerSecond));
+    WheelRadiusCharacterizationState state = new WheelRadiusCharacterizationState();
+
+    return Commands.parallel(
+            // Drive control sequence
+            Commands.sequence(
+                // Reset acceleration limiter
+                Commands.runOnce(() -> limiter.reset(0.0)),
+
+                // Turn in place, accelerating up to full speed
+                Commands.run(
+                    () ->
+                        this.applyRequest(
+                            () ->
+                                new FieldCentric()
+                                    .withSteerRequestType(SteerRequestType.Position)
+                                    .withRotationalRate(
+                                        limiter.calculate(
+                                            DriveConstants.WHEEL_RADIUS_TEST_MAX_VELOCITY.in(
+                                                RadiansPerSecond)))),
+                    this)),
+
+            // Measurement sequence
+            Commands.sequence(
+                // Wait for modules to fully orient before starting measurement
+                Commands.waitSeconds(1.0),
+
+                // Record starting measurement
+                Commands.runOnce(
+                    () -> {
+                      state.startingWheelPositions[0] =
+                          this.getModule(0).getDriveMotor().getPosition().getValue().in(Radians);
+                      state.startingWheelPositions[1] =
+                          this.getModule(1).getDriveMotor().getPosition().getValue().in(Radians);
+                      state.startingWheelPositions[2] =
+                          this.getModule(2).getDriveMotor().getPosition().getValue().in(Radians);
+                      state.startingWheelPositions[3] =
+                          this.getModule(3).getDriveMotor().getPosition().getValue().in(Radians);
+                      state.lastHeading = this.getState().Pose.getRotation();
+                      state.headingDelta = 0.0;
+                    }),
+
+                // Update gyro delta
+                Commands.run(
+                    () -> {
+                      Rotation2d rotation = this.getState().Pose.getRotation();
+                      state.headingDelta +=
+                          Math.abs(rotation.minus(state.lastHeading).getRadians());
+                      state.lastHeading = rotation;
+
+                      double[] positions = new double[4];
+                      positions[0] =
+                          this.getModule(0).getDriveMotor().getPosition().getValue().in(Radians);
+                      positions[1] =
+                          this.getModule(1).getDriveMotor().getPosition().getValue().in(Radians);
+                      positions[2] =
+                          this.getModule(2).getDriveMotor().getPosition().getValue().in(Radians);
+                      positions[3] =
+                          this.getModule(3).getDriveMotor().getPosition().getValue().in(Radians);
+
+                      double wheelDelta = 0.0;
+                      for (int i = 0; i < 4; i++) {
+                        wheelDelta +=
+                            Math.abs(positions[i] - state.startingWheelPositions[i]) / 4.0;
+                      }
+
+                      double wheelRadius =
+                          (state.headingDelta * DriveConstants.DRIVETRAIN_RADIUS.in(Inches))
+                              / wheelDelta;
+
+                      SmartDashboard.getNumber(
+                          "Wheel Radius Characterization/Wheel Delta", wheelDelta);
+                      SmartDashboard.getNumber(
+                          "Wheel Radius Characterization/Wheel Radius", wheelRadius);
+                    })))
+        .withName("Wheel Radius Characterization Test");
   }
 }

930051f39fdeb11e5224909c08a07ef26ade99b2
William Crouch <richardwilliamcrouch@gmail.com>
2026-03-13 20:06:30 -0400

    Uses multitag results if available

--- src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/VisionSubsystem.java
@@ -14,6 +14,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import frc.robot.generated.TunerConstants;
 import frc.robot.statemachines.DriveState;
+import java.util.List;
+import org.photonvision.targeting.MultiTargetPNPResult;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
@@ -70,66 +72,109 @@ public class VisionSubsystem extends SubsystemBase {
       // This is FIFO, so the oldest is given first and the newest last'
       var frontCameraResults = CameraConstants.photonCamera_Front.getAllUnreadResults();
       for (var result : frontCameraResults)
-        createMeasurement(result, CameraConstants.photonCameraName_Front);
+        evaluateResult(result, CameraConstants.photonCameraName_Front);
 
       // Left Camera
       var leftCameraResults = CameraConstants.photonCamera_Left.getAllUnreadResults();
       for (var result : leftCameraResults)
-        createMeasurement(result, CameraConstants.photonCameraName_Left);
+        evaluateResult(result, CameraConstants.photonCameraName_Left);
 
       // Right Camera
       var rightCameraResults = CameraConstants.photonCamera_Right.getAllUnreadResults();
       for (var result : rightCameraResults)
-        createMeasurement(result, CameraConstants.photonCameraName_Right);
+        evaluateResult(result, CameraConstants.photonCameraName_Right);
     }
   }
 
   // code from PoseEstimator class
-  private void createMeasurement(PhotonPipelineResult result, String cameraName) {
-    PhotonTrackedTarget lowestAmbiguityTarget = null;
+  private void evaluateResult(PhotonPipelineResult result, String cameraName) {
 
-    double lowestAmbiguityScore = 10;
-    for (PhotonTrackedTarget target : result.targets) {
-      double targetPoseAmbiguity = target.getPoseAmbiguity();
-      // Make sure the target is a Fiducial target.
-      if (targetPoseAmbiguity != -1 && targetPoseAmbiguity < lowestAmbiguityScore) {
-        lowestAmbiguityScore = targetPoseAmbiguity;
-        lowestAmbiguityTarget = target;
+    if (!result.multitagResult.isEmpty()
+        && result.multitagResult.get().estimatedPose.ambiguity
+            < CameraConstants.MAXIMUM_ALLOWED_AMBIGUITY) {
+      MultiTargetPNPResult multiTargetPNPResult = result.multitagResult.get();
+      Pose3d estimatedPose =
+          Pose3d.kZero
+              .plus(multiTargetPNPResult.estimatedPose.best)
+              .relativeTo(CameraConstants.FIELD_LAYOUT.getOrigin())
+              .plus(CameraConstants.cameraTransformMap.get(cameraName).inverse());
+      evaluateEstimation(estimatedPose, multiTargetPNPResult.fiducialIDsUsed, cameraName);
+    } else if (result.hasTargets()) {
+      PhotonTrackedTarget lowestAmbiguityTarget = null;
+      double lowestAmbiguityScore = 10;
+
+      for (PhotonTrackedTarget target : result.targets) {
+        double targetPoseAmbiguity = target.getPoseAmbiguity();
+        // Make sure the target is a Fiducial target.
+        if (targetPoseAmbiguity != -1 && targetPoseAmbiguity < lowestAmbiguityScore) {
+          lowestAmbiguityScore = targetPoseAmbiguity;
+          lowestAmbiguityTarget = target;
+        }
       }
+
+      // Although there are confirmed to be targets, none of them may be fiducial
+      // targets.
+      if (lowestAmbiguityScore > CameraConstants.MAXIMUM_ALLOWED_AMBIGUITY) return;
+
+      int targetFiducialId = lowestAmbiguityTarget.getFiducialId();
+
+      Pose3d targetPosition = CameraConstants.FIELD_LAYOUT.getTagPose(targetFiducialId).get();
+
+      evaluateEstimation(
+          targetPosition
+              .transformBy(lowestAmbiguityTarget.getBestCameraToTarget().inverse())
+              .transformBy(CameraConstants.cameraTransformMap.get(cameraName).inverse()),
+          targetFiducialId,
+          cameraName);
     }
+  }
 
-    // Although there are confirmed to be targets, none of them may be fiducial
-    // targets.
-    if (lowestAmbiguityTarget == null
-        || lowestAmbiguityScore > CameraConstants.MAXIMUM_ALLOWED_AMBIGUITY) return;
+  // evaluates the estimations before export
+  private void evaluateEstimation(Pose3d pose, List<Short> targetsUsed, String cameraName) {
 
-    int targetFiducialId = lowestAmbiguityTarget.getFiducialId();
+    double estimateDistance =
+        driveStats.Pose.getTranslation().getDistance(pose.getTranslation().toTranslation2d());
+    if (!RobotModeTriggers.disabled().getAsBoolean()
+        && estimateDistance > TunerConstants.kSpeedAt12Volts.in(MetersPerSecond) * 0.02) return;
 
-    Pose3d targetPosition = CameraConstants.FIELD_LAYOUT.getTagPose(targetFiducialId).get();
+    double averageRobotToTagDistance = 0;
+    for (int target : targetsUsed)
+      averageRobotToTagDistance +=
+          pose.getTranslation()
+                  .getDistance(
+                      CameraConstants.FIELD_LAYOUT.getTagPose(target).get().getTranslation())
+              / targetsUsed.size();
 
-    evaluateMeasurement(
-        targetPosition
-            .transformBy(lowestAmbiguityTarget.getBestCameraToTarget().inverse())
-            .transformBy(CameraConstants.cameraTransformMap.get(cameraName).inverse()),
-        lowestAmbiguityTarget,
+    double xyStdDev =
+        CameraConstants.XY_STD_DEV_COEFFICIENT
+            * Math.pow(averageRobotToTagDistance, 1.2)
+            / Math.pow(targetsUsed.size(), 2);
+
+    double thetaStdDev =
+        CameraConstants.THETA_STD_DEV_COEFFICIENT
+            * Math.pow(averageRobotToTagDistance, 1.2)
+            / Math.pow(targetsUsed.size(), 2);
+
+    driveState.addVisionEstimate(
+        new VisionMeasurement(
+            pose.toPose2d(),
+            Utils.getCurrentTimeSeconds(),
+            VecBuilder.fill(xyStdDev, xyStdDev, thetaStdDev)),
         cameraName);
   }
 
   // evaluates the estimations before export
-  private void evaluateMeasurement(Pose3d pose, PhotonTrackedTarget targetUsed, String cameraName) {
+  private void evaluateEstimation(Pose3d pose, int targetUsed, String cameraName) {
 
     double estimateDistance =
         driveStats.Pose.getTranslation().getDistance(pose.getTranslation().toTranslation2d());
+    if (!RobotModeTriggers.disabled().getAsBoolean()
+        && estimateDistance > TunerConstants.kSpeedAt12Volts.in(MetersPerSecond) * 0.02) return;
+
     double robotToTagDistance =
         pose.getTranslation()
             .getDistance(
-                CameraConstants.FIELD_LAYOUT
-                    .getTagPose(targetUsed.getFiducialId())
-                    .get()
-                    .getTranslation());
-
-    if (!RobotModeTriggers.disabled().getAsBoolean()
-        && estimateDistance > TunerConstants.kSpeedAt12Volts.in(MetersPerSecond) * 0.02) return;
+                CameraConstants.FIELD_LAYOUT.getTagPose(targetUsed).get().getTranslation());
 
     double xyStdDev = CameraConstants.XY_STD_DEV_COEFFICIENT * Math.pow(robotToTagDistance, 1.2);
 
